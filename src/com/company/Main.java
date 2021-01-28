2a3c1ea86935685ce2dd67355013f5e7b20ed7fc
Atri Hegde <iamatrihegde@gmail.com>
2021-01-28 20:58:34 +0000

    task set

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
 public class Main {
+    /*
+    Program which manages IT repairs to devices. It can:
+    --> Enter a new device
+    --> Assign the device to a user
+    --> Raise issues with a device
+    --> Track issues with a device
+    --> Track the repair status of a device
+    --> Assign a repair to a technician
+    --> Update the status of a device
+    --> Remove a device from the system
+     */
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello world lmao");
     }
 }
